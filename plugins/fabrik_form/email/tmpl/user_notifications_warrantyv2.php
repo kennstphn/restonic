f836dd887ba13a48fbce21f214cda5764ff182af
Chad Windnagle <drmmr763@gmail.com>
2014-10-21 16:56:25 -0400

    add in warranty email notification text from Restonic

--- plugins/fabrik_form/email/tmpl/user_notifications_warrantyv2.php
+++ plugins/fabrik_form/email/tmpl/user_notifications_warrantyv2.php
@@ -13,8 +13,58 @@ $warrantyLogic = new warrantyLogic($this->data);
 // call to helper function file method
 $isValidClaim = $warrantyLogic->isValidClaim();
 
-if ($isValidClaim): ?>
-    <p>Valid Claim</p>
+// get form form data
+$factoryName = $this->data['tbl_warranty_claim_oct___factory_locations_db'];
+
+// form will return an array so knock it down to the first option
+$factoryName = $factoryName[0];
+
+// if we are the other option lets switch out for the other field from the form
+if ($factoryName == 'Other')
+{
+    $factoryName = $this->data['tbl_warranty_claim_oct___other_location'];
+}
+
+
+?>
+<p>Hi, {tbl_warranty_claim_oct___first_name}</p>
+<?php if ($isValidClaim): ?>
+    <p>
+    Thank you for submitting a warranty claim with Restonic Mattress Company.
+    Your claim has been submitted and someone will be in touch within two business days.
+    </p>
+    <p>
+        Please remember that you’ll need:
+        <ul>
+            <li>Your original sales receipt</li>
+            <li>The law tag located on the end of your mattress</li>
+        </ul>
+    </p>
+    <p>
+    While Restonic maintains the registry, the law label affords you warranty rights and
+    the sales receipt validates you are the original purchaser.
+    </p>
+    <p>
+    Thank you for contacting Restonic and allowing us to continue to serve you.
+    </p>
+
 <?php else: ?>
-    <p>Invalid Claim</p>
+    <p>Thank you for submitting a warranty claim with Restonic Mattress Company.</p>
+
+    <p>
+        We noticed that you’re missing your original receipt or law tag on
+        your claim form, which means your claim cannot be processed.
+    </p>
+    <p>
+        While Restonic maintains the registry, the law label affords you warranty rights and the sales
+        receipt validates you are the original purchaser. If you’d like to follow up with <?php echo $factoryName; ?>,
+        please visit us online and choose the appropriate manufacturer to contact.
+    </p>
+    <p>
+        Again, thank you for contacting Restonic and allowing us to continue to serve you.
+    </p>
 <?php endif; ?>
+<p>
+    Sweet Dreams, <br />
+    Restonic Mattress Company
+</p>
